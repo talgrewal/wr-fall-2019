d28a7edd916320d86c75da5b720ffa0ae0f92dfe
greywall <talwindergrewal01@gmail.com>
2019-12-03 15:27:29 -0800

    Add Account Form

--- client/js/components/AccountForm/AccountForm.js
+++ client/js/components/AccountForm/AccountForm.js
@@ -1,3 +1,26 @@
 import React, {Component} from 'react';
 import styles from './styles';
-// import {Form, Field} from 'react-final-form';
+import {Form, Field} from 'react-final-form';
+import {Text, View} from 'react-native';
+
+class AccountForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formToggle: true,
+      error: null,
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Login here</Text>
+        <Text>Username: ________</Text>
+        <Text>Password: ________</Text>
+      </View>
+    );
+  }
+}
+
+export default AccountForm;
--- client/js/screens/Login/Login.js
+++ client/js/screens/Login/Login.js
@@ -2,6 +2,8 @@ import React from 'react';
 import AccountFrom from '../../components/AccountForm';
 import styles from './styles';
 
-const Login = ({classes}) => {
-  return <></>;
+const Login = () => {
+  return <AccountFrom />;
 };
+
+export default Login;
