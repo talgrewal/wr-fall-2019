4c9997034e4d67a1e266f29ba6285fea999a8b97
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-12-05 16:42:00 -0800

    add styles to background of drawer nav

--- client/js/navigation/DrawerScreen.js
+++ client/js/navigation/DrawerScreen.js
@@ -15,20 +15,34 @@ class DrawerScreen extends Component {
 
   render() {
     return (
-      <SafeAreaView style={{marginStart: 20}}>
+      <SafeAreaView
+        style={{
+          backgroundColor: 'black',
+          height: '100%',
+          width: '100%',
+          marginLeft: 0,
+        }}>
         <TouchableOpacity
           onPress={() => this.navigateToScreen('About')}
           style={{marginTop: 80}}>
-          <Text style={{fontSize: 20, padding: 25}}>About Us</Text>
+          <Text style={{fontSize: 20, padding: 25, color: 'white'}}>
+            About Us
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => this.navigateToScreen('Contact')}>
-          <Text style={{fontSize: 20, padding: 25}}>Contact Us</Text>
+          <Text style={{fontSize: 20, padding: 25, color: 'white'}}>
+            Contact Us
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => this.navigateToScreen('Terms')}>
-          <Text style={{fontSize: 20, padding: 25}}>Terms & Conditions</Text>
+          <Text style={{fontSize: 20, padding: 25, color: 'white'}}>
+            Terms & Conditions
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => this.navigateToScreen('Privacy')}>
-          <Text style={{fontSize: 20, padding: 25}}>Privacy Policy</Text>
+          <Text style={{fontSize: 20, padding: 25, color: 'white'}}>
+            Privacy Policy
+          </Text>
         </TouchableOpacity>
       </SafeAreaView>
     );
