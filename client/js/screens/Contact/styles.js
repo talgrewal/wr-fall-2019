12d3e318d0caa78fa0241f73c54609b32c893082
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-12-06 11:57:43 -0800

    add data and styles to Contact Us page

--- client/js/screens/Contact/styles.js
+++ client/js/screens/Contact/styles.js
@@ -1,3 +1,36 @@
 import {StyleSheet} from 'react-native';
-const styles = StyleSheet.create({});
+import {typography} from '../../config/styles';
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  heading: {
+    fontSize: 24,
+    fontFamily: typography.fontMainLight,
+    fontWeight: 'bold',
+    paddingBottom: 10,
+  },
+  imageOne: {
+    height: 60,
+    width: 80,
+  },
+  imageTwo: {
+    height: 95,
+    width: 80,
+  },
+  imageThree: {
+    height: 60,
+    width: 80,
+  },
+  headerView: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 20,
+  },
+  bodyText: {paddingVertical: 25, fontFamily: typography.fontMainLight},
+  bodyTextTwo: {
+    paddingVertical: 40,
+    fontFamily: typography.fontMainLight,
+  },
+});
 export default styles;
