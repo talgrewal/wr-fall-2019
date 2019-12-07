e7c7bfa8488b6705a6c5b7efa17f02f3bb9d1d27
greywall <talwindergrewal01@gmail.com>
2019-12-07 15:16:09 -0800

    Add signout button for auth delete

--- client/js/screens/Home/Home.js
+++ client/js/screens/Home/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, TouchableOpacity} from 'react-native';
 
 const Home = props => {
-  return <Text>Home Screen</Text>;
+  return (
+    <>
+      <Text>Home Screen</Text>
+      <TouchableOpacity onPress={console.log('Sign out under construction!')}>
+        <Text>Sign out!</Text>
+      </TouchableOpacity>
+    </>
+  );
 };
 
 export default Home;
