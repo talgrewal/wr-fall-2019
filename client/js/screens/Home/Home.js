adec22a8df90d0488634f7b19b5e4df0eea8dda4
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-12-10 11:58:13 -0800

    add politico category

--- client/js/screens/Home/Home.js
+++ client/js/screens/Home/Home.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import styles from './styles';
 
 const Home = props => {
-  return <Text>Home Screen</Text>;
+  return (
+    <ScrollView style={styles.container}>
+      <Text>Campaign Categories</Text>
+      <View style={styles.headerView}>
+        <Image
+          style={styles.headerImage}
+          source={require('../../assets/category/Politico.png')}
+        />
+      </View>
+    </ScrollView>
+  );
 };
 
 export default Home;
