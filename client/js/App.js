539e815842bdc68631dde708b6d010eac7b227c8
Ciaran Fontein <ciaranfontein@gmail.com>
2019-12-10 09:56:24 -0800

    Subscribtion confirmation styled and is visible by default and hides on tap

--- client/js/App.js
+++ client/js/App.js
@@ -8,7 +8,7 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <Confirmation title="campagin name" />
+        <Confirmation title="Save the Whales" />
         <RootStackNavigator />
       </>
     );
--- client/js/screens/Confirmation/Confirmation.js
+++ client/js/screens/Confirmation/Confirmation.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, Image, Modal} from 'react-native';
 import styles from './styles';
-const Confirmation = ({title}) => {
-  return (
-    <View style={styles.page}>
-      <View style={styles.container}>
-        <Text style={styles.title}>{title}</Text>
-      </View>
-    </View>
-  );
-};
+import checkmark from '../../assets/miscicons/Check.png';
+import {TouchableHighlight} from 'react-native-gesture-handler';
+class Confirmation extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    modalVisible: true,
+  };
+
+  hideModal = () => {
+    this.setState({modalVisible: false});
+  };
+  render() {
+    const {title} = this.props;
+    return (
+      <Modal
+        transparent="true"
+        visible={this.state.modalVisible}
+        animationType="fade">
+        <TouchableHighlight onPress={this.hideModal}>
+          <View style={styles.page}>
+            <View style={styles.container}>
+              <Image source={checkmark} />
+              <Text style={styles.title}>{title}</Text>
+              <Text style={styles.text}>
+                You have successfully subscribed to this campaign!
+              </Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+      </Modal>
+    );
+  }
+}
 
 export default Confirmation;
