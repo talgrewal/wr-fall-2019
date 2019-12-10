76cac41908f1d294ed690eaf38553a28dd57576d
Ciaran Fontein <ciaranfontein@gmail.com>
2019-12-10 10:01:40 -0800

    Refactored terms styles to use globals theme

--- client/js/screens/Terms/styles.js
+++ client/js/screens/Terms/styles.js
@@ -1,19 +1,23 @@
 import {StyleSheet} from 'react-native';
-import {typography} from '../../config/styles';
+import {THEME} from '../../config/Theme';
+const {
+  typography: {mainFont, hugeText},
+  colors: {darkGrey},
+} = THEME;
 const styles = StyleSheet.create({
   container: {
     padding: 20,
   },
   heading: {
-    fontSize: 24,
-    fontFamily: typography.fontMainLight,
+    fontSize: hugeText,
+    fontFamily: mainFont,
     fontWeight: '800',
     paddingBottom: 10,
   },
   description: {
-    fontFamily: typography.fontMainLight,
-    fontSize: 14,
-    color: '#2B2B2B',
+    fontFamily: mainFont,
+    fontSize: contentText,
+    color: darkGrey,
     lineHeight: 18,
   },
 });
