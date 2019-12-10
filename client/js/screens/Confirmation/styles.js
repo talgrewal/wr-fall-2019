95f33e6e3eed8192853cfe7eaa46b0e62f902d54
Ciaran Fontein <ciaranfontein@gmail.com>
2019-12-10 09:56:24 -0800

    Subscribtion confirmation styled and is visible by default and hides on tap

--- client/js/screens/Confirmation/styles.js
+++ client/js/screens/Confirmation/styles.js
@@ -2,17 +2,32 @@ import {StyleSheet} from 'react-native';
 import {THEME} from '../../config';
 
 const {
-  typography: {headerFont, hugeText},
+  typography: {headerFont, mainFont, hugeText},
+  formatting: {centeredChildren, containerWidth, baseSpacing},
+  colors: {white},
 } = THEME;
 const styles = StyleSheet.create({
   page: {
     height: '100%',
     width: '100%',
+    ...centeredChildren,
+    backgroundColor: 'rgba(0,0,0, 0.59)',
   },
-  container: {},
+  container: {
+    width: containerWidth,
+    ...centeredChildren,
+    borderRadius: 10,
+    backgroundColor: white,
+    padding: baseSpacing * 4,
+  },
   title: {
     fontFamily: headerFont,
     fontSize: hugeText,
+    padding: baseSpacing,
+  },
+  text: {
+    textAlign: 'center',
+    fontFamily: mainFont,
   },
 });
 export default styles;
