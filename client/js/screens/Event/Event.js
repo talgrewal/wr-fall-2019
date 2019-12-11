2463f1cb59d8967f5c51555898bb9aa214e67ef0
Ciaran Fontein <ciaranfontein@gmail.com>
2019-12-11 11:00:47 -0800

    Basic dummy calendar event is able to be added

--- client/js/screens/Event/Event.js
+++ client/js/screens/Event/Event.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import {Text, View, Image, TextInput} from 'react-native';
+import {Text, View, Image, TextInput, ScrollView} from 'react-native';
 import eventImage from '../../assets/Imagery/ManAndTree.png';
 import commentIcon from '../../assets/miscicons/question.png';
 import styles from './styles';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import Comment from '../../components/Comment';
 import dot from '../../assets/artwork/blackspot.png';
+import * as AddCalendarEvent from 'react-native-add-calendar-event';
+import moment from 'moment';
 
 const Event = ({title, address, city, date, time, description, comments}) => {
+  const nowUTC = moment.utc();
+
+  const [value, onChangeText] = React.useState('');
   let commentItems = [];
   for (let i = 0; i < comments.length; i++) {
     commentItems.push(
-      <View style={styles.commentContainer}>
+      <View key={i} style={styles.commentContainer}>
         <Image style={styles.bullet} source={dot} />
         <Comment
-          key={i}
           user={comments[i].user}
           comment={comments[i].comment}
           date={comments[i].date}
@@ -22,8 +26,9 @@ const Event = ({title, address, city, date, time, description, comments}) => {
       </View>,
     );
   }
+
   return (
-    <View style={styles.page}>
+    <ScrollView style={styles.page}>
       <View style={styles.container}>
         <Text style={styles.title}>{title}</Text>
         <View style={styles.infoContainer}>
@@ -33,7 +38,11 @@ const Event = ({title, address, city, date, time, description, comments}) => {
           <Text style={styles.text}>{time}</Text>
         </View>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              addToCalendar(title, nowUTC);
+            }}>
             <Text style={styles.buttonText}>Add to my calendar</Text>
           </TouchableOpacity>
         </View>
@@ -48,13 +57,41 @@ const Event = ({title, address, city, date, time, description, comments}) => {
             placeholderTextColor="black"
             style={styles.input}
             onChangeText={text => onChangeText(text)}
+            value={value}
           />
           <Image style={styles.commentIcon} source={commentIcon} />
         </View>
         <View style={styles.comments}>{commentItems}</View>
       </View>
-    </View>
+    </ScrollView>
   );
+};
+const utcDateToString = momentInUTC => {
+  let s = moment.utc(momentInUTC).format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
+  // console.warn(s);
+  return s;
+};
+
+addToCalendar = (title, startDateUTC) => {
+  const eventConfig = {
+    title,
+    startDate: utcDateToString(startDateUTC),
+    endDate: utcDateToString(moment.utc(startDateUTC).add(1, 'hours')),
+    notes: 'tasty!',
+    navigationBarIOS: {
+      tintColor: 'orange',
+      backgroundColor: 'green',
+      titleColor: 'blue',
+    },
+  };
+  console.log('Before library call');
+  AddCalendarEvent.presentEventCreatingDialog(eventConfig)
+    .then(eventInfo => {
+      console.warn(JSON.stringify(eventInfo));
+    })
+    .catch(error => {
+      console.warn(error);
+    });
 };
 
 export default Event;
