342fa777d54dbe93217ffaad0d3ecff8d9947485
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-12-11 13:20:04 -0800

    add styles to the My campaign screen

--- client/js/screens/MyCampaigns/styles.js
+++ client/js/screens/MyCampaigns/styles.js
@@ -1,3 +1,52 @@
 import {StyleSheet} from 'react-native';
-const styles = StyleSheet.create({});
+import {THEME} from '../../config';
+const {
+  colors: {black, darkGrey, white},
+
+  typography: {
+    headerFont,
+    hugeText,
+    contentText,
+    mainFont,
+    subHeaderText,
+    smallText,
+    italicFont,
+  },
+} = THEME;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  pageTitle: {
+    fontSize: hugeText,
+    fontFamily: headerFont,
+    margin: 20,
+  },
+  item: {
+    backgroundColor: darkGrey,
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: contentText,
+    fontFamily: mainFont,
+    color: white,
+  },
+  category: {
+    fontSize: smallText,
+    color: white,
+    fontFamily: italicFont,
+  },
+  panel: {
+    borderRadius: 5,
+
+    height: '100%',
+    width: '100%',
+  },
+});
 export default styles;
