25fad84d632f922ac1377bed013a81f35cd6dbb0
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-12-11 13:15:15 -0800

    modify component for small subscribe button

--- client/js/components/SmallSubscribeButton/SmallSubscribeButton.js
+++ client/js/components/SmallSubscribeButton/SmallSubscribeButton.js
@@ -1,2 +1,17 @@
 import React, {Component} from 'react';
 import styles from './styles';
+import {TouchableOpacity, Text} from 'react-native';
+
+const SmallSubscribeButton = props => {
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => {
+        console.log('pressed subscribed');
+      }}>
+      <Text style={styles.buttonText}>Subscribe</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default SmallSubscribeButton;
--- client/js/components/SmallSubscribeButton/styles.js
+++ client/js/components/SmallSubscribeButton/styles.js
@@ -1,5 +1,32 @@
 import {StyleSheet} from 'react-native';
-
-const styles = StyleSheet.create({});
+import {THEME} from '../../config';
+const {
+  colors: {black, darkGrey, white},
+  formatting: {baseSpacing, centeredChildren, containerWidth},
+  typography: {
+    baseSize,
+    headerFont,
+    hugeText,
+    contentText,
+    mainFont,
+    subHeaderText,
+  },
+} = THEME;
+const styles = StyleSheet.create({
+  button: {
+    borderColor: white,
+    borderWidth: 1,
+    borderRadius: baseSpacing * 2,
+    padding: baseSpacing,
+    padding: baseSpacing,
+    height: 50,
+    width: 104, //Width 100% does not work
+  },
+  buttonText: {
+    fontFamily: headerFont,
+    fontSize: 12,
+    color: white,
+  },
+});
 
 export default styles;
