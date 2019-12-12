8bde369f564d3956021fcef490b569741b9f3e5b
greywall <talwindergrewal01@gmail.com>
2019-12-11 17:26:25 -0800

    Remove extra links from home screen

--- client/js/screens/Home/Home.js
+++ client/js/screens/Home/Home.js
@@ -47,40 +47,6 @@ const Home = ({navigation}) => {
         />
       </View>
 
-      <View>
-        <View>
-          <Text>This is the Home Screen</Text>
-          <Text>Click on the links below:</Text>
-          <TouchableOpacity
-            onPress={() =>
-              navigation.navigate('Campaigns', {campaignName: 'Enviro'})
-            }>
-            <Text>Go to the Enviro Page</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() =>
-              navigation.navigate('Campaigns', {campaignName: 'Social'})
-            }>
-            <Text>Go to the Social Page</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() =>
-              navigation.navigate('Campaigns', {campaignName: 'Politico'})
-            }>
-            <Text>Go to the Politico Page</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() =>
-              navigation.navigate('Campaigns', {campaignName: 'Charity'})
-            }>
-            <Text>Go to the Charity Page</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('Campaign')}>
-            <Text>Go to the Campaign Page</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
       <View style={styles.container}>
         <Text style={styles.categoryTitle}>Campaign Categories</Text>
         <ScrollView>
