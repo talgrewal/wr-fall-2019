76a33517a1195da5899b0d31723814c3221c352e
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-12-11 16:07:15 -0800

    remove console.logs and commented out code

--- client/js/components/SmallUnsubscribeButton/SmallUnsubscribeButton.js
+++ client/js/components/SmallUnsubscribeButton/SmallUnsubscribeButton.js
@@ -4,11 +4,7 @@ import {TouchableOpacity, Text} from 'react-native';
 
 const SmallUnsubscribeButton = props => {
   return (
-    <TouchableOpacity
-      style={styles.button}
-      onPress={() => {
-        console.log('pressed subscribed');
-      }}>
+    <TouchableOpacity style={styles.button} onPress={() => {}}>
       <Text style={styles.buttonText}>Unsubscribe</Text>
     </TouchableOpacity>
   );
