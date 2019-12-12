15b05e6351a795ab9e60b7d98a57651db29bb113
greywall <talwindergrewal01@gmail.com>
2019-12-11 20:00:09 -0800

    Add mutation to add campaign subscription

--- client/js/components/MainSubscribeButton/MainSubscribeButton.js
+++ client/js/components/MainSubscribeButton/MainSubscribeButton.js
@@ -2,16 +2,46 @@ import React, {Component} from 'react';
 import styles from '../MainSigninButton/styles';
 import {TouchableOpacity, View, ImageBackground, Text} from 'react-native';
 import InactiveButton from '../../assets/buttons/Inactivespacebutton.png';
+import gql from 'graphql-tag';
+import ApolloClient from 'apollo-boost';
+import {Mutation} from '@apollo/react-components';
+import client from '../../config/api';
+
+const CAMPAIGN_SUBSCRIBE = gql`
+  mutation updateUser {
+    updateUser(
+      data: {campaigns: {connect: {id: "ck3z9pk3wcyey0b094u0yg3sb"}}}
+      where: {id: "ck3uqo41p5j6n0b00dus4dfu9"}
+    ) {
+      id
+      name
+      campaigns {
+        id
+        title
+      }
+    }
+  }
+`;
 
 const MainSubscribeButton = () => {
   return (
-    <View style={styles.buttonHolder}>
-      <TouchableOpacity style={styles.button}>
-        <ImageBackground source={InactiveButton} style={styles.buttonImage}>
-          <Text style={styles.text}>Subscribe</Text>
-        </ImageBackground>
-      </TouchableOpacity>
-    </View>
+    <Mutation mutation={CAMPAIGN_SUBSCRIBE} client={client}>
+      {updateUser => {
+        console.log(updateUser);
+        console.log(client);
+        return (
+          <View style={styles.buttonHolder}>
+            <TouchableOpacity onPress={updateUser} style={styles.button}>
+              <ImageBackground
+                source={InactiveButton}
+                style={styles.buttonImage}>
+                <Text style={styles.text}>Subscribe</Text>
+              </ImageBackground>
+            </TouchableOpacity>
+          </View>
+        );
+      }}
+    </Mutation>
   );
 };
 
