4fb8770be49130e4be8ce936dec90bcfdf6ec5ed
greywall <talwindergrewal01@gmail.com>
2019-12-13 16:36:19 -0800

    Remove unused variable

--- client/js/components/MainSubscribeButton/MainSubscribeButton.js
+++ client/js/components/MainSubscribeButton/MainSubscribeButton.js
@@ -23,7 +23,7 @@ const CAMPAIGN_SUBSCRIBE = gql`
   }
 `;
 
-const MainSubscribeButton = ({CampaignId, ViewerId, userId}) => {
+const MainSubscribeButton = ({CampaignId, userId}) => {
   return (
     <Mutation mutation={CAMPAIGN_SUBSCRIBE} client={client}>
       {updateUser => {
