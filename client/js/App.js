d8b15a9ab7421bc91061612de36d98a0d977a2e8
Ciaran Fontein <ciaranfontein@gmail.com>
2019-12-17 13:31:32 -0800

    Resolved merge conflict removed user provider

--- client/js/App.js
+++ client/js/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import RootStackNavigator from './navigation/RootStackNavigator';
-import UserProvider from './context/UserProvider';
 import client from './config/api';
 import {ApolloProvider} from 'react-apollo';
 
