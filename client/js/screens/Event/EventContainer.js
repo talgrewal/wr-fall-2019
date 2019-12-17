8f8fb9de920651ce4d142f8f63adc563b132af1f
Ciaran Fontein <ciaranfontein@gmail.com>
2019-12-17 13:29:12 -0800

    Resolved merge conflict removing user context

--- client/js/screens/Event/EventContainer.js
+++ client/js/screens/Event/EventContainer.js
@@ -1,16 +1,46 @@
 import React, {Component} from 'react';
 import Event from './Event';
-import {UserContext} from '../../context/UserProvider';
-import {withNavigation} from 'react-navigation';
+import {queryViewer} from '../../config/models';
 
-class EventContainer extends Component {
+export default class EventContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+  }
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  getUser = async () => {
+    const user = await queryViewer();
+    this.setState({user});
+  };
+
   render() {
+    const {
+      title,
+      description,
+      location,
+      startDate,
+      endDate,
+      comments,
+    } = this.props;
+
     return (
-      <UserContext.Consumer>
-        {({user}) => <Event user={user} />}
-      </UserContext.Consumer>
+      this.state.user && (
+        <Event
+          title={title}
+          description={description}
+          location={location}
+          startDate={startDate}
+          endDate={endDate}
+          comments={comments}
+          user={this.state.user}
+        />
+      )
     );
   }
 }
-
-export default withNavigation(EventContainer);
