85f1eda078e038417b5511fb4bdae07c9e35eea5
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-12-17 10:50:02 -0800

    add back button on specefic routes

--- client/js/navigation/config.js
+++ client/js/navigation/config.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, TouchableOpacity, Image} from 'react-native';
 import {Header} from 'react-navigation-stack';
+import styles from './styles';
 
 const AppHeader = props => (
   <View
@@ -13,9 +14,9 @@ const AppHeader = props => (
     <Image
       style={{
         position: 'absolute',
-        top: 40,
+        top: 65,
         left: 65,
-        height: '40%',
+        height: '30%',
         width: '70%',
         resizeMode: 'contain',
       }}
@@ -27,34 +28,35 @@ const AppHeader = props => (
 
 const MenuButton = ({navigation}) => {
   return (
-    <View style={{justifyContent: 'space-between'}}>
-      <TouchableOpacity onPress={navigation.openDrawer}>
-        <Image
-          style={{height: 25, width: 35, resizeMode: 'contain', marginLeft: 20}}
-          source={require('../assets/headingelement/Burgermenu.png')}
-        />
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Image
-          style={{
-            height: 20,
-            width: 20,
-            resizeMode: 'contain',
-            marginLeft: 20,
-          }}
-          source={require('../assets/headingelement/backarrow.png')}
-        />
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity onPress={navigation.openDrawer}>
+      <Image
+        style={{height: 25, width: 35, resizeMode: 'contain', marginLeft: 20}}
+        source={require('../assets/headingelement/Burgermenu.png')}
+      />
+    </TouchableOpacity>
   );
 };
+
+const BackButton = ({navigation}) => (
+  <TouchableOpacity onPress={() => navigation.goBack()}>
+    <Image
+      style={{
+        height: 30,
+        width: 30,
+        resizeMode: 'contain',
+        marginLeft: 20,
+        marginTop: 5,
+      }}
+      source={require('../assets/headingelement/backarrow.png')}
+    />
+  </TouchableOpacity>
+);
 
 const ProfileButton = ({navigation}) => {
   return (
     <TouchableOpacity>
       <Image
-        style={{height: 25, width: 25, resizeMode: 'contain', marginRight: 20}}
+        style={{height: 30, width: 30, resizeMode: 'contain', marginRight: 20}}
         source={require('../assets/headingelement/Signedin.png')}
       />
     </TouchableOpacity>
@@ -64,11 +66,19 @@ const ProfileButton = ({navigation}) => {
 export const sharedNavigationOptions = navigation => ({
   headerBackTitle: null,
   header: props => <AppHeader {...props} />,
-  headerLeft: props => <MenuButton {...props} navigation={navigation} />,
+  headerLeft: props =>
+    navigation.state.routeName === 'Campaigns' ||
+    navigation.state.routeName === 'Campaign' ||
+    navigation.state.routeName === 'Events' ? (
+      <BackButton {...props} navigation={navigation} />
+    ) : (
+      <MenuButton {...props} navigation={navigation} />
+    ),
 
   headerRight: props => <ProfileButton {...props} navigation={navigation} />,
 
   headerStyle: {
     backgroundColor: 'transparent',
+    height: 70,
   },
 });
