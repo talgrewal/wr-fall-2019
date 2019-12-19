62e3c5a5430d0692cdeeff49abc63160d41295da
Ciaran Fontein <ciaranfontein@gmail.com>
2019-12-18 16:09:22 -0800

    Fresh install to remove yarn lock etc

--- client/js/screens/Event/EventContainer.js
+++ client/js/screens/Event/EventContainer.js
@@ -1,6 +1,22 @@
 import React, {Component} from 'react';
 import Event from './Event';
 import {queryViewer} from '../../config/models';
+import gql from 'graphql-tag';
+import {Query} from '@apollo/react-components';
+import {Text} from 'react-native';
+import Loader from '../../components/Loader';
+
+export const GET_COMMENTS = gql`
+  query($eventid: ID!) {
+    event(where: {id: $eventid}) {
+      comments {
+        username
+        comment
+        createdAt
+      }
+    }
+  }
+`;
 
 export default class EventContainer extends Component {
   constructor(props) {
@@ -20,26 +36,25 @@ export default class EventContainer extends Component {
   };
 
   render() {
-    const {
-      title,
-      description,
-      location,
-      startDate,
-      endDate,
-      comments,
-    } = this.props;
-
+    const id = this.props.navigation.state.params.event.id;
     return (
       this.state.user && (
-        <Event
-          title={title}
-          description={description}
-          location={location}
-          startDate={startDate}
-          endDate={endDate}
-          comments={comments}
-          user={this.state.user}
-        />
+        <Query
+          query={GET_COMMENTS}
+          variables={{eventid: id}}
+          fetchPolicy="network-only">
+          {({loading, error, data, refetch}) => {
+            if (error) return <Text>error</Text>;
+            if (loading) return <Loader />;
+            return (
+              <Event
+                comments={data.event.comments.reverse()}
+                user={this.state.user}
+                refetch={refetch}
+              />
+            );
+          }}
+        </Query>
       )
     );
   }
